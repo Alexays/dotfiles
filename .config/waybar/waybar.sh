d57fcd4bc60c2aa013a74f557a57cd76828574fd
Alexis <alexisr245@gmail.com>
2018-08-19 14:33:23 +0200

    chore: update dotfiles

---
+++ .config/waybar/waybar.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q waybar
+
+# Wait until the processes have been shut down
+while pgrep -x waybar >/dev/null; do sleep 1; done
+
+# Launch main
+cd $HOME/Project/Waybar
+./build/waybar &
